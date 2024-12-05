57855c74724dedc9a9d1eec8c267103f7a23dc38
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2024-12-04 16:15:24 -0800

    implemented binary search on a sorted array

--- 2-Arrays.ts
+++ 2-Arrays.ts
@@ -16,3 +16,33 @@ console.log(nums);
 // delete //
 nums.splice(4, 1);
 console.log(nums);
+
+const binarySearch = (
+  arr: number[],
+  target: number,
+  start: number = 0,
+  end: number = arr.length - 1
+): boolean => {
+  // base case //
+  if (start > end) return false;
+
+  const mid = Math.floor((start + end) / 2);
+
+  // found //
+  if (arr[mid] === target) {
+    return true;
+  }
+
+  // search left half //
+  if (target < arr[mid]) {
+    return binarySearch(arr, target, start, mid - 1);
+  }
+
+  // search right half //
+  return binarySearch(arr, target, mid + 1, end);
+};
+
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(binarySearch(arr, 5));
+console.log(binarySearch(arr, 10));
+console.log(binarySearch(arr, 3));
